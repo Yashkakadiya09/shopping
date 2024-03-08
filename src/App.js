ff8d0e5e315fa722c0cdf5670a3a8cf28425e2e6
YashKakadiya09 <yashkakadiyayk89@gmail.com>
2024-03-08 12:59:10 +0530

    add somethig

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Cart from "./Components/Cart";
-
 import Electronics from "./Components/catogary/Electronics";
-
 import SingleProduct from "./Components/Card/SingleProduct";
 import CheckOut from "./Components/CheckOut";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Login/Signup";
 import Profile from "./Components/Login/Profile";
-
 import PaymentPage from "./Components/PaymentPage";
-
 import Furniture from "./Components/catogary/Furniture";
 import Shoes from "./Components/catogary/Shoes";
 import Clothes from "./Components/catogary/Clothes";
 import Miscellaneous from "./Components/catogary/Miscellaneous";
-
 import ScrollToTop from "./ScrollToTop";
-import { useEffect, useState } from "react";
 
 function App() {
   // const [user, setUser] = useState();
